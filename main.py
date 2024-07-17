f435bb603fea56ab13095c8ba18dbdeacb1df553
jordan baumann <0501jordanb@gmail.com>
2024-07-17 14:16:33 -0400

    A bit of sdl stuff

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
+import sys
+import sdl2.ext
+
 def Main():
     print("Greetings universe and awesomers!\n")
+
+    sdl2.ext.init()
+
+    window = sdl2.ext.Window("Hello World!", size=(640, 480))
+    window.show()
+
+    factory = sdl2.ext.SpriteFactory(sdl2.ext.SOFTWARE)
+
+    spriterenderer = factory.create_sprite_render_system(window)
+
+    while True:
+        pass
+
+    print("Farewell universe!\n")
 
 Main()
